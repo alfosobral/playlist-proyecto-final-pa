54f4a6b0dd032b0d3a826816040b465979cff8da
alfosobral <alfonsosobral@gmail.com>
2025-11-11 09:01:01 -0300

    Proyecto base Spring Boot Mi Playlist

---
+++ src/main/java/com/playlist/pa/playlist_pa/Application.java
@@ -0,0 +1,13 @@
+package com.playlist.pa.playlist_pa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
